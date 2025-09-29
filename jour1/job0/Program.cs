451e4f5117c8715a26ba8e413e67d7f96410d774
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-09-29 23:47:41 +0200

    feat: ajout du job 0 du jour 1

---
+++ jour1/job0/Program.cs
@@ -0,0 +1,45 @@
+/*
+ * Auteur : Lorenzo OTTAVIANI.
+ * Date : 29/09/2025 23h35
+ * But de l'exercice :
+ *    Déclarer et afficher des variables de chaque type primitif.
+ * Entrée : ∅
+ * Sortie : Affichage des variables.
+ */
+
+using System;
+
+namespace job0
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            char lettre = 'a';
+            Console.WriteLine(lettre);
+            
+            string mot = "pomme";
+            Console.WriteLine(mot);
+            
+            int petit = 15;
+            Console.WriteLine(petit);
+            
+            long grand = 2000;
+            Console.WriteLine(grand);
+            
+            float oligo = 8.25f;
+            Console.WriteLine(oligo);
+            
+            double multi = 8.25;
+            Console.WriteLine(multi);
+            
+            bool test = false;
+            Console.WriteLine(test);
+            
+            Console.Write("Test de l'int TOTO 12,25 : ");
+            // int TOTO = 12.25;
+            // Console.WriteLine(TOTO);
+            Console.Write("Non autorisé !!");
+        }
+    }
+} 
