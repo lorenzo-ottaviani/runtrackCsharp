cb84e280d481e3f7447ba58a8babb24c0a6ccb1b
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-10-03 17:43:59 +0200

    feat: ajout du job 4, jour 1

--- jour1/job4/Program.cs
+++ jour1/job4/Program.cs
@@ -1,8 +1,17 @@
-﻿namespace job4;
+﻿/*
+ * Auteur : Lorenzo OTTAVIANI.
+ * Date : 03/10/2025 17h41
+ * But de l'exercice :
+ *    Affiche le carré d'un nombre entré par l'utilisateur.
+ * Entrée : Un nombre entier.
+ * Sortie : Affichage du carré du nombre.
+ */
+
+namespace job4;
 
 class Program
 {
-    int Carree(int n)
+    static int Carree(int n)
     {
         return n * n;
     }
@@ -10,16 +19,25 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Choisir un nombre : ");
-
-        try
-        {
-            string? entree = Console.ReadLine();
-            int nombre = int.Parse(entree);
-        }
-        catch
-        {
-            Console.WriteLine("Nombre invalide");
-        }
+        string? entree = Console.ReadLine();
         
+        if (!string.IsNullOrWhiteSpace(entree))
+        {
+            try
+            { 
+                int nombre = int.Parse(entree);
+                int res = Carree(nombre);
+                Console.WriteLine($"Le carrée de ton nombre est : {res}");
+            }
+            catch (FormatException e)
+            {
+             Console.WriteLine("\nNombre invalide !");
+             Console.WriteLine(e);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Tu n'as rien entré !");
+        }
     }
 }
